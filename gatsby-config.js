357c27fd538bf028fc91022a13d0b71208d462ee
Ashwanth A R <ashwanth1109@gmail.com>
2020-05-10 01:18:30 +0530

    feature: basic header with searchbar ui

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,26 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-typescript`, `gatsby-plugin-emotion`],
+  plugins: [
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-emotion`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 590,
+              withWebp: true,
+            },
+          },
+        ],
+      },
+    },
+  ],
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,8 @@
 import React from "react"
 import styled from "@emotion/styled"
+import Logo from "../../static/logo.png"
+import Search from "../icons/search"
+import { purple } from "../styles/colors"
 
 const Container = styled.div`
   width: 100%;
@@ -17,8 +20,79 @@ const Container = styled.div`
   }
 `
 
+const Section = styled.div`
+  position: relative;
+  height: 100%;
+  img {
+    height: 80%;
+  }
+
+  h1 {
+    margin-left: 8px;
+  }
+`
+
+const SearchBar = styled.input`
+  background: whitesmoke;
+  width: 100%;
+  max-width: 400px;
+  height: 40px;
+  border: none;
+  border-radius: 8px;
+  outline: none;
+  font-size: 1rem;
+  line-height: 2.25rem;
+  padding: 0 16px 0 40px;
+
+  :focus {
+    background: white;
+    -webkit-box-shadow: inset 0px 0px 0px 2px ${purple.C40};
+    -moz-box-shadow: inset 0px 0px 0px 2px ${purple.C40};
+    box-shadow: inset 0px 0px 0px 2px ${purple.C40};
+  }
+`
+
+const ThemeButton = styled.div``
+
+const SearchContainer = styled.div`
+  max-width: 400px;
+  width: 100%;
+`
+
+const IconContainer = styled.div`
+  position: absolute;
+  left: 0;
+  height: 100%;
+  width: 40px;
+
+  svg {
+    width: 16px;
+    height: 16px;
+  }
+`
+
 const Header = () => {
-  return <Container className="pad-h">Header</Container>
+  return (
+    <Container className="pad-h df j-between">
+      <Section className="df a-center">
+        <img src={Logo} alt="A dog smiling in a party hat" />
+        <h1 className="font-s">Code Ninja</h1>
+      </Section>
+      <Section className="df a-center flex j-end">
+        <SearchContainer className="relative">
+          <IconContainer className="df a-center j-center">
+            <Search />
+          </IconContainer>
+          <SearchBar
+            type="text"
+            placeholder="Search for tags"
+            className="flex"
+          />
+        </SearchContainer>
+        <ThemeButton />
+      </Section>
+    </Container>
+  )
 }
 
 export default Header
