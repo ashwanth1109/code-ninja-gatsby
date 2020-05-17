50d3bed0780fa56a27768d380fbb22967daecdef
Ashwanth A R <ashwanth1109@gmail.com>
2020-05-17 09:43:43 +0530

    build: fix build issue

--- src/components/sidenav.tsx
+++ src/components/sidenav.tsx
@@ -6,16 +6,6 @@ const Container = styled.div`
   width: 280px;
   max-width: 280px;
   position: relative;
-  &:after {
-    background-color: #f0f0f2;
-    bottom: 0rem;
-    content: "";
-    width: 1px;
-    top: 0rem;
-    position: absolute;
-    right: 0rem;
-    z-index: -1;
-  }
 `
 
 const Sidenav = () => {
